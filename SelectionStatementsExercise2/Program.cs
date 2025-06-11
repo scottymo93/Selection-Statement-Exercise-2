263377cf14a1749698a30e39380bb7887bb7cbbd
Scott Dyrness <scottdyrness@gmail.com>
2025-06-11 15:21:48 -0700

    completed selection statements exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+               case "math":
+                   Console.WriteLine("Math is cool, I love numbers!");
+                   break;
+               case "science":
+                   Console.WriteLine("Science is really interesting!");
+                   break;
+               case "history":
+                   Console.WriteLine("History! I love learning about the ancient ways of life!");
+                   break;
+               case "reading":
+                   Console.WriteLine("Reading is fun! I love adventure books!");
+                   break;
+               case "lunch":
+                   Console.WriteLine("Yummy yummy in the tummy!");
+                   break;
+               default: 
+                   Console.WriteLine($"Wow that is interesting! What do you like about {favoriteSubject}?"); 
+                   break;
+            
+            }
         }
     }
 }
